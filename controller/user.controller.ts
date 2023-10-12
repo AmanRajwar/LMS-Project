72804128413e611c5a7fd343187954245af2160b
AmanRajwar <amanrajwar79@gmail.com>
2023-10-12 21:04:55 +0530

    Logout and Social auth

--- controller/user.controller.ts
+++ controller/user.controller.ts
@@ -4,13 +4,13 @@ import { Request, Response, NextFunction } from "express";
 import userModel, { IUser } from "../models/user.model";
 import ErrorHandler from "../utils/ErrorHandler";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
-import jwt, { Secret } from "jsonwebtoken";
+import jwt, { JwtPayload, Secret } from "jsonwebtoken";
 import ejs from 'ejs';
 import path from "path";
 import sendMail from "../utils/sendMail";
-import { Error } from "mongoose";
-import { sendToken } from "../utils/jwt";
+import { accessTokenOptions, sendToken, refreshTokenOptions } from "../utils/jwt";
 import { redis } from "../utils/redis";
+import { getUserById } from "../services/user.service";
 
 interface IRegistrationBody {
     name: string;
@@ -92,7 +92,6 @@ interface IActivationRequest {
 export const activateUser = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { activation_code, activation_token } = req.body as IActivationRequest;
-        console.log(req.body);
 
         const newUser: { user: IUser; activationCode: string } = jwt.verify(
             activation_token,
@@ -184,5 +183,88 @@ export const authorizeRoles = (...roles: string[]) => {
         next();
     }
 }
+
+//update access token
+export const updateAccessToken = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const refresh_token = req.cookies.refresh_token as string;
+        const decoded = jwt.verify(refresh_token, process.env.REFRESH_TOKEN as string) as JwtPayload;
+
+
+        const message = 'Could not refresh token';
+        if (!decoded) {
+            return next(new ErrorHandler(message, 400));
+        }
+        const session = await redis.get(decoded.id as string);
+        if (!session) {
+            return next(new ErrorHandler(message, 400));
+        }
+
+        const user = JSON.parse(session);
+
+        const accessToken = jwt.sign(
+            { id: user._id },
+            process.env.ACCESS_TOKEN as string,
+            {
+                expiresIn: "5m"
+            })
+
+        const refreshToken = jwt.sign(
+            { id: user._id },
+            process.env.REFRESH_TOKEN as string,
+            {
+                expiresIn: '3d',
+            })
+
+        res.cookie("access_token", accessToken, accessTokenOptions)
+        res.cookie("refresh_token", refreshToken, refreshTokenOptions)
+
+        res.status(200).json({
+            status: "success",
+            accessToken
+        })
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400))
+    }
+})
+
+
+//get user info
+export const getUserInfo = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const userId = req.user?._id;
+        getUserById(userId, res);
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400))
+    }
+})
+interface ISocialAuthBody {
+    email: string;
+    name: string;
+    avatar: string;
+}
+//social auth
+export const socialAuth = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { email, name, avatar } = req.body as ISocialAuthBody;
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            const newUser = await userModel.create({
+                email,
+                name,
+                avatar
+            })
+            console.log("aman",newUser)
+            sendToken(newUser, 200, res);
+        }else{
+            sendToken(user, 200, res);
+        }
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400))
+    }
+})
+
+
+
 
 
--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { activateUser, authorizeRoles, loginUser, logoutUser, registrationUser } from "../controller/user.controller";
+import { activateUser, authorizeRoles, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken } from "../controller/user.controller";
 import { isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
 
@@ -10,5 +10,10 @@ userRouter.post('/activate-user', activateUser);
 userRouter.post('/login', loginUser);
 
 userRouter.get('/logout', isAuthenticated, logoutUser);
+userRouter.get('/refresh', updateAccessToken);
 
+userRouter.get('/me', isAuthenticated,getUserInfo);
+
+
+userRouter.post('/social-auth',socialAuth);
 export default userRouter;

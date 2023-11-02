78da40c5e08675a5db689fca73edbf80bd85db3b
AmanRajwar <amanrajwar79@gmail.com>
2023-11-02 17:47:31 +0530

    get all courses functionality complete

--- routes/course.route.ts
+++ routes/course.route.ts
@@ -1,20 +1,33 @@
 import express from "express";
 import { isAuthenticated } from "../middleware/auth";
 import { authorizeRoles } from "../controller/user.controller";
-import { editCourse, uploadCourse } from "../controller/course.controller";
+import { editCourse, getAllCourses, getSingleCourse, uploadCourse } from "../controller/course.controller";
 const courseRouter = express.Router();
 
-courseRouter.post('/create-course',
+courseRouter.post(
+    '/create-course',
     isAuthenticated,
     authorizeRoles('admin'),
     uploadCourse
 );
 
-courseRouter.put('/edit-course',
+courseRouter.put(
+    '/edit-course/:id',
     isAuthenticated,
     authorizeRoles('admin'),
     editCourse
 );
+
+courseRouter.get(
+    '/get-course/:id',
+    getSingleCourse
+);
+
+courseRouter.get(
+    '/get-courses',
+    getAllCourses
+);
+
 
 export default courseRouter;
 

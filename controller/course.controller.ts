623611b25169e13de2fcffe4e1de51a682438ef1
AmanRajwar <amanrajwar79@gmail.com>
2023-12-02 14:09:09 +0530

    Delete all notification using cron job done

--- controller/course.controller.ts
+++ controller/course.controller.ts
@@ -9,6 +9,7 @@ import mongoose from "mongoose";
 import path from "path";
 import ejs from "ejs";
 import { sendMail } from "../utils/sendMail";
+import NotificationModel from "../models/notificationModel";
 
 
 //upload course
@@ -192,6 +193,11 @@ export const addQuestion = CatchAsyncError(async (req: Request, res: Response, n
         //add this question to our course content 
         courseContent.questions.push(newQuestion);
 
+        await NotificationModel.create({
+            user: req.user?._id,
+            title: "New Question Received",
+            message: `You have a new question from ${courseContent.title}`
+        });
         // save the updated course
         await course?.save();
 
@@ -247,6 +253,11 @@ export const addAnswer = CatchAsyncError(async (req: Request, res: Response, nex
 
         if (req.user?._id === question.user._id) {
             //create a notification 
+            await NotificationModel.create({
+                user:req.user?._id,
+                title:"New Question Reply Received",
+                message:`You have new question reply in ${courseContent.title}`
+            })
         } else {
             const data = {
                 name: question.user.name,
--- controller/notification.controller.ts
+++ controller/notification.controller.ts
@@ -2,15 +2,7 @@ import NotificationModel from "../models/notificationModel"
 import { Request, Response, NextFunction, response } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import ErrorHandler from "../utils/ErrorHandler";
-import cloudinary from "cloudinary"
-import { createCourse } from "../services/course.service";
-import CourseModel from "../models/course.model";
-import { redis } from "../utils/redis";
-import mongoose from "mongoose";
-import path from "path";
-import ejs from "ejs";
-import { sendMail } from "../utils/sendMail";
-
+import cron from "node-cron";
 
 export const getNotification = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -47,6 +39,13 @@ export const updateNotification = CatchAsyncError(async (req: Request, res: Resp
     } catch (error: any) {
         return next(new ErrorHandler(error.message, 500))
     }
+});
+
+// delete notification --- only admin
+cron.schedule("0 0 0 * * *", async () => {
+    const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+    await NotificationModel.deleteMany({ status: "read", createdAt: { $lt: thirtyDaysAgo } })
+    console.log("Deleted read notifications")
 })
 
 

ad9489cc64709c2dda4920ba755a35f0aef572e3
amanrajwar <amanrajwar79@gmail.com>
2024-01-17 13:36:58 +0530

    get layout and edit layout is also complete

--- routes/layout.route.ts
+++ routes/layout.route.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { isAuthenticated } from "../middleware/auth";
 import { authorizeRoles } from "../controller/user.controller";
-import { createLayout } from "../controller/layout.controller";
+import { createLayout, editLayout, getLayoutByType } from "../controller/layout.controller";
 const layoutRouter = express.Router();
 
 layoutRouter.post("/create-layout",isAuthenticated,authorizeRoles("admin"),createLayout);
+layoutRouter.put("/edit-layout",isAuthenticated,authorizeRoles("admin"),editLayout);
+layoutRouter.get("/get-layout",getLayoutByType);
 
 export default layoutRouter;
